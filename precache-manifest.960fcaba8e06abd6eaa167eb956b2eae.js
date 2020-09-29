be35125c588fff037472646f0a0c496d18e3574b
LeoLiu <you@example.com>
2020-09-29 20:40:42 +0800

    Updates

---
+++ precache-manifest.960fcaba8e06abd6eaa167eb956b2eae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19946be2bfd9161449450a5f27ba44db",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "2e9957474805f1198420",
+    "url": "/my-resume/static/css/main.f6de89f2.chunk.css"
+  },
+  {
+    "revision": "e00aef513f00ee599cff",
+    "url": "/my-resume/static/js/2.5380133b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-resume/static/js/2.5380133b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9957474805f1198420",
+    "url": "/my-resume/static/js/main.8552505d.chunk.js"
+  },
+  {
+    "revision": "a6efcd828d3624d74f01",
+    "url": "/my-resume/static/js/runtime-main.fb0541e1.js"
+  },
+  {
+    "revision": "bc52a216d370e1e4d6581a0a1b639af1",
+    "url": "/my-resume/static/media/kururu.bc52a216.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
