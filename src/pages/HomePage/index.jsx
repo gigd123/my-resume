43a687f3f00fddd2118a313b1329ce6228289922
LeoLiu <leoliu@iMac.local>
2020-09-29 13:43:05 +0800

    add json

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,22 +5,22 @@ import worksData from '../works.json';
 
 const HomePage = () => {
   return <div className={styles.wrap}>
-    <section className={ styles.brief }>
-      <img src={ require("../../assets/images/kururu.jpg") } />
+    <section className={styles.brief}>
+      <img src={require("../../assets/images/kururu.jpg")} />
       <h2>LeoLiu</h2>
       <ul>
-          <li>front-end engineer</li>
+        <li>front-end engineer</li>
       </ul>
       <p>I am a front-end developer.</p>
       <p>Contact me at test@gmail.com</p>
-    </section>  
+    </section>
 
-    <section className={ styles.work }>
-      <div className={ styles.title }>
+    <section className={styles.work}>
+      <div className={styles.title}>
         <h3>A selection of work</h3>
         <p>You can find more in the work overview.</p>
       </div>
-      <WorkLists workListData={ worksData.data } />
+      <WorkLists workListData={worksData.data} />
     </section>
   </div>
 }
